d2fea800fe7434816a977fefe660a8f14f8fdbee
Beando <33060724+Beando@users.noreply.github.com>
2020-02-16 18:24:30 +0000

    Added Gathering
    Added gathering so Civilians can gather but cannot drop off at point yet.

--- Unity RTS/Assets/Scripts/ObjectInfo.cs
+++ Unity RTS/Assets/Scripts/ObjectInfo.cs
@@ -5,21 +5,33 @@ using UnityEngine.AI;
 
 public class ObjectInfo : MonoBehaviour
 {
+    
+    public NodeManager.ResourceTypes heldResourceType;
+
     public bool isSelected = false;
+    public bool isGathering = false;
 
     public string objectName;
+
+    public int heldResource;
+    public int maxHeldResource;
 
     private NavMeshAgent agent;
 
     // Start is called before the first frame update
     void Start()
     {
+        StartCoroutine(GatherTick());
         agent = GetComponent<NavMeshAgent>(); 
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(heldResource >= maxHeldResource)
+        {
+            //Return to drop off point
+        }
         if (Input.GetMouseButtonDown(1) && isSelected)
         {
             RightClick();
@@ -37,7 +49,46 @@ public class ObjectInfo : MonoBehaviour
             {
                 agent.destination = hit.point;
                 Debug.Log("Moving");
+            }else if(hit.collider.tag == "Resource")
+            {
+                agent.destination = hit.collider.gameObject.transform.position;
+                Debug.Log("Harvesting");
             }
+        }
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        GameObject hitObject = other.gameObject;
+
+        if(hitObject.tag == "Resource")
+        {
+            isGathering = true;
+            hitObject.GetComponent<NodeManager>().gatherers++;
+            heldResourceType = hitObject.GetComponent<NodeManager>().resourceType;
+        }
+    }
+    public void OnTriggerExit(Collider other)
+    {
+        GameObject hitObject = other.gameObject;
+        
+        if(hitObject.tag == "Resource")
+        {
+            hitObject.GetComponent<NodeManager>().gatherers--;
+
+        }
+    }
+
+    IEnumerator GatherTick()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(1);
+            if (isGathering)
+            {
+                heldResource++;
+            }
+            
         }
     }
 }
